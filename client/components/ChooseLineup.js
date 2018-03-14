7c5a5d677516db9d8879fa98a14b67ab5b4a49db
Ari Kramer <arikramer24@gmail.com>
2018-03-14 16:53:21 -0400

    set up full card on lineup page

--- client/components/ChooseLineup.js
+++ client/components/ChooseLineup.js
@@ -5,13 +5,13 @@ import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-ho
 import { fetchUserBatters } from '../store';
 import { SingleBatter } from './index';
 
-const SortableItem = SortableElement(({ value, spot, clickStats, idx }) => {
+const Batter = SortableElement(({ value, spot, clickStats, idx }) => {
   const { firstName, lastName, image, position } = value;
   let place;
-  !isNaN(Number(spot)) ? place = (Number(spot) + 1) : place = spot;
+  !isNaN(Number(spot)) ? place = ((Number(spot) + 1) + '.') : place = spot;
   return (
     <div>
-      <p>{place}. {firstName + ' ' + lastName}, {position} <button onClick={() => clickStats(idx)}>Stats</button></p>
+      <p>{place} {firstName + ' ' + lastName}, {position} <button className={`lineup-full-card-button-${idx}`} onClick={() => clickStats(idx, true)}>Full Card</button></p>
       {/*<img src={image} className="player-img" />*/}
     </div>
   )
@@ -21,15 +21,15 @@ const SortableItem = SortableElement(({ value, spot, clickStats, idx }) => {
 const Lineup = SortableContainer((props) => {
   const { batters, clickStats } = props;
   return (
-    <ul>
+    <div id="choose-lineup-lineup">
       {batters.map((batter, index) => {
         let spot;
-        index < 9 ? spot = index : spot = 'Bench'
+        index < 9 ? spot = index : spot = 'Bench:'
         return (
-          <SortableItem key={`item-${index}`} index={index} spot={spot} idx={index} value={batter} clickStats={clickStats} />
+          <Batter key={`item-${index}`} index={index} spot={spot} idx={index} value={batter} clickStats={clickStats} />
         )
       })}
-    </ul>
+    </div>
   );
 });
 
@@ -45,27 +45,27 @@ class ChooseLineup extends Component {
   }
 
   componentDidMount() {
-    this.setState({batters: this.props.activeUser.batters})
+    this.setState({ batters: this.props.activeUser.batters })
   }
-  
+
   onSortEnd = ({ oldIndex, newIndex }) => {
     this.setState({
       batters: arrayMove(this.state.batters, oldIndex, newIndex),
     });
   };
 
-  clickStats(idx) {
-    this.setState({ displayStats: true, playerToDisplay: this.state.batters[idx] })
+  clickStats(idx, bool) {
+    this.setState({ displayStats: bool, playerToDisplay: this.state.batters[idx] })
   }
 
   render() {
-    console.log(this.state)
+    // console.log(this.state)
     return (
       <div id="choose-lineup">
-      <Lineup batters={this.state.batters} onSortEnd={this.onSortEnd} axis={'xy'} clickStats={this.clickStats} />
-      {
-        this.state.displayStats && <SingleBatter isLineup={true} thisBatter={this.state.playerToDisplay} />
-      }
+        <Lineup batters={this.state.batters} onSortEnd={this.onSortEnd} axis={'xy'} clickStats={this.clickStats} />
+        {
+          this.state.displayStats && <SingleBatter isLineup={true} thisBatter={this.state.playerToDisplay} clickStats={this.clickStats} />
+        }
       </div>
     )
   }
--- client/components/SingleBatter.js
+++ client/components/SingleBatter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchBatter, /*fetchUsers*/ } from '../store';
+import onClickOutside from 'react-onclickoutside';
 
 class SingleBatter extends Component {
   constructor() {
@@ -26,9 +27,19 @@ class SingleBatter extends Component {
     }
   }
 
+  handleClickOutside(evt) {
+    const targetClass = evt.target.classList.value;
+    if (targetClass.startsWith('lineup-full-card-button')) {
+      const id = Number(targetClass.slice(24))
+      this.props.clickStats(id, true)
+    } else {
+      this.props.clickStats(null, false)
+    }
+  };
+
   render() {
     const { singleBatter, thisBatter, isLineup, playerId } = this.props;
-    let batter; 
+    let batter;
     let id;
     let lineupClass;
     if (isLineup) {
@@ -44,21 +55,32 @@ class SingleBatter extends Component {
 
       return (
         <div className={lineupClass}>
-          {
-            isLineup && [
-              <p key={1}>{batter.name}</p>,
-              <p key={2}>On-Base: {batter.onBase}</p>
-            ]
-          }
-          <p>Strikeout: {this.convertArray(batter.SO)}</p>
-          <p>Groundout: {this.convertArray(batter.GB)}</p>
-          <p>Flyout: {this.convertArray(batter.FB)}</p>
-          <p>Walk: {this.convertArray(batter.BB)}</p>
-          <p>Single: {this.convertArray(batter.single)}</p>
-          <p>Single-Plus: {this.convertArray(batter.singlePlus)}</p>
-          <p>Double: {this.convertArray(batter.double)}</p>
-          <p>Triple: {this.convertArray(batter.triple)}</p>
-          <p>Home Run: {this.convertArray(batter.homeRun)}</p>
+          <div className="single-batter-column-container">
+            {
+              isLineup && <h2>{batter.name}</h2>
+            }
+            <div className="single-batter-info">
+              <div className="single-batter-column">
+                {
+                  isLineup && <p>On-Base: {batter.onBase}</p>
+                }
+                <p>Strikeout: {this.convertArray(batter.SO)}</p>
+                <p>Groundout: {this.convertArray(batter.GB)}</p>
+                <p>Flyout: {this.convertArray(batter.FB)}</p>
+                <p>Walk: {this.convertArray(batter.BB)}</p>
+              </div>
+              <div className="single-batter-column">
+                <p>Single: {this.convertArray(batter.single)}</p>
+                <p>Single-Plus: {this.convertArray(batter.singlePlus)}</p>
+                <p>Double: {this.convertArray(batter.double)}</p>
+                <p>Triple: {this.convertArray(batter.triple)}</p>
+                <p>Home Run: {this.convertArray(batter.homeRun)}</p>
+              </div>
+            </div>
+          </div>
+          <div className="single-batter-column">
+            <img src={batter.image} className="player-img" />
+          </div>
 
         </div>
 
@@ -83,4 +105,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SingleBatter));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(onClickOutside(SingleBatter)));
