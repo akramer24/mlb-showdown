021db1d3bb038541f017765c9b99fddf27fab6fd
Ari Kramer <arikramer24@gmail.com>
2018-03-20 15:15:50 -0400

    navbar dropdown fade in/out

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { logout, fetchUserBatters, fetchUserPitchers } from '../store';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import navTo from './utils/navTo';
 import onClickOutside from 'react-onclickoutside';
 
@@ -11,13 +9,13 @@ class NavBar extends React.Component {
     super();
     this.state = {
       didLoad: false,
-      displayRightDropdown: false
+      displayRightDropdown: false,
+      fade: 'fadeIn'
     }
     this.displayDropdown = this.displayDropdown.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log('load')
     const { loadUserPlayers, isLoggedIn, activeUser } = nextProps;
     if (isLoggedIn && !this.state.didLoad) {
       loadUserPlayers(Number(activeUser.userInfo.id), true);
@@ -26,82 +24,64 @@ class NavBar extends React.Component {
   }
 
   displayDropdown(dropdown, bool) {
-    this.setState({ [dropdown]: bool })
+    if (bool) {
+      this.setState({ [dropdown]: !this.state.displayRightDropdown, fade: 'fadeIn' })
+    }
+    else {
+      this.setState({ fade: 'fadeOut' })
+      setTimeout(() => {
+        this.setState({ [dropdown]: !this.state.displayRightDropdown })
+      }, 700)
+    }
   }
 
-  handleClickOutside = evt => {
-    this.setState({ displayRightDropdown: false })
+  handleClickOutside = () => {
+    this.setState({ fade: 'fadeOut' })
+    setTimeout(() => {
+      this.setState({ displayRightDropdown: false, fade: 'fadeIn' })
+    }, 700)
   };
 
   render() {
     const { handleLogout, isLoggedIn, activeUser } = this.props;
-
     return (
       <div id="navbar">
-        <div id="navbar-title">
-          <h3 id="navbar-title-text" className="navbar-text" onClick={() => navTo('/')}>MLB SHOWDOWN</h3>
-        </div>
-        <div id="navbar-options">
-          {
-            isLoggedIn &&
-            <p onClick={() => navTo('/quick-game')} className="navbar-text">Quick Game</p>
-          }
+        <div id="navbar-left">
+          <div id="navbar-title">
+            <h3 id="navbar-title-text" className="navbar-text" onClick={() => navTo('/')}>MLB SHOWDOWN</h3>
+          </div>
+          <div id="navbar-options">
+            {
+              isLoggedIn &&
+              <p onClick={() => navTo('/quick-game')} className="navbar-text">Quick Game</p>
+            }
+          </div>
         </div>
         <div id="navbar-right">
           <p className="navbar-text" onClick={() => this.displayDropdown('displayRightDropdown', !this.state.displayRightDropdown)}>{activeUser.userInfo.teamName}</p>
           {
             this.state.displayRightDropdown &&
-            <div id="navbar-right-dropdown" className="animated fadeIn">
-              <p onClick={() => navTo(`/users/${activeUser.userInfo.id}`)} className="navbar-text">My Team</p>
-              <br />
-              <p className="navbar-text" onClick={handleLogout}>Log Out</p>
+            <div id="navbar-right-dropdown" className={`animated ${this.state.fade}`}>
+              <p
+                onClick={() => {
+                  navTo(`/users/${activeUser.userInfo.id}`)
+                  this.displayDropdown('displayRightDropdown', !this.state.displayRightDropdown)
+                }
+                }
+                className="navbar-text">My Team</p>
+              <p
+                className="navbar-text"
+                onClick={() => {
+                  handleLogout()
+                  this.displayDropdown('displayRightDropdown', !this.state.displayRightDropdown)
+                }
+                }>Log Out</p>
             </div>
           }
         </div>
       </div>
     )
   }
-
-  // render() {
-  //   const { handleLogout, isLoggedIn, activeUser } = this.props;
-  //   // console.log(Number(activeUser.userInfo.id))
-  //   return (
-  //     <Navbar collapseOnSelect>
-  //       <Navbar.Header>
-  //         <Navbar.Brand>
-  //           <NavLink to="/">MLB SHOWDOWN</NavLink>
-  //         </Navbar.Brand>
-  //         <Navbar.Toggle />
-  //       </Navbar.Header>
-  //       <Navbar.Collapse>
-  //         <Nav>
-  //           <NavDropdown eventKey={1} title="Players" id="basic-nav-dropdown">
-  //             <MenuItem eventKey={1.1}>All</MenuItem>
-  //             <MenuItem eventKey={1.2} onClick={() => navTo('/players/batters')}>Batters</MenuItem>
-  //             <MenuItem eventKey={1.3} onClick={() => navTo('/players/pitchers')}>Pitchers</MenuItem>
-  //           </NavDropdown>
-  //           {
-  //             isLoggedIn
-  //             &&
-  //             <NavItem onClick={() => navTo('/quick-game')}>Quick Game</NavItem>
-  //           }
-  //         </Nav>
-  //         <Nav pullRight>
-  //           {
-  //             isLoggedIn
-  //               ?
-  //               <NavDropdown eventKey={2} title={activeUser.userInfo.teamName} id="basic-nav-dropdown">
-  //                 <MenuItem eventKey={2.1} onClick={() => navTo(`/users/${activeUser.userInfo.id}`)}>My team</MenuItem>
-  //                 <MenuItem eventKey={2.2} onClick={handleLogout}>Log Out</MenuItem>
-  //               </NavDropdown>
-  //               :
-  //               <NavItem onClick={() => navTo('/login')}>Log In</NavItem>
-  //           }
-  //         </Nav>
-  //       </Navbar.Collapse>
-  //     </Navbar>
-  //   )
-  // }
 }
 
 /**
