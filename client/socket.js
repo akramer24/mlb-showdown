253dd04d770833ea04457f70ecc4f675839959d9
Ari Kramer <arikramer24@gmail.com>
2018-03-21 18:06:21 -0400

    display all logged in users except active user

--- client/socket.js
+++ client/socket.js
@@ -6,7 +6,8 @@ const socket = io(window.location.origin);
 socket.on('connect', () => {
   console.log('Connected!')
   socket.on('add online user', onlineUsers => {
-    store.dispatch(addOnlineUser(onlineUsers))
+    const userTeam = store.getState().user.activeUser.userInfo.teamName;
+    store.dispatch(addOnlineUser(onlineUsers.filter(team => team !== userTeam)))
   })
   socket.on('online user removed', onlineUsers => {
     store.dispatch(removeOnlineUser(onlineUsers))
