3b067a7d23c7163de24681f45e456614f27571b2
Ari Kramer <arikramer24@gmail.com>
2018-04-09 15:39:06 -0400

    updated user again

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
+const User = require('./user');
 const Batter = require('./Batter');
 const Pitcher = require('./Pitcher');
 const UserBatter = require('./UserBatter');
